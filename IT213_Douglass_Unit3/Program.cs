e4036e1c61ae1bf664b7864f4889982a5c947d7c
Xenos <Xenos@FBIVAN>
2021-07-12 20:19:09 -0400

    Add project files.

---
+++ IT213_Douglass_Unit3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IT213_Douglass_Unit3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*********************************************************
+
+            //****Assignment 3 Section 1
+
+            //*********************************************************
+
+            // Cycle through 10 iterations of while loop
+
+            int i = 0;
+            int sumGrade = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("Please enter a grade: ");
+                int grade = Convert.ToInt32(Console.ReadLine());
+                
+                sumGrade += grade;
+                Console.WriteLine("Grade: " + grade);
+                Console.WriteLine("Counter: " + i);
+                i++;
+
+                if (i < 11)
+                {
+                    Console.WriteLine("You total grade is: " + sumGrade);
+                    Console.WriteLine("Your average grade is: " + sumGrade / 10);
+                }
+            }
+
+            
+            
+        }   
+    }
+}
